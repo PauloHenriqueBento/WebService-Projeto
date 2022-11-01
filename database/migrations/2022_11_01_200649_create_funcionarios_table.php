563f7465b4a15758eb712933fde23f069eff09db
PauloHenriqueBentoDosSantos <phbento@outlook.com.br>
2022-11-01 17:49:11 -0300

    migration criada

---
+++ database/migrations/2022_11_01_200649_create_funcionarios_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funcionarios', function (Blueprint $table) {
+            $table->id();
+            $table->string("nome");
+            $table->date("dataNasc")->nullable();;
+            $table->string("telefone")->nullable();
+            $table->string("email")->nullable();
+
+            $table->unsignedBigInteger("departamento_id")->nullable();
+            $table->foreign("departamento_id")
+                            ->references("id")
+                            ->on("departamentos")
+                            ->onUpdate('cascade')
+                            ->onDelete("cascade")
+                            ->nullable();
+
+            $table->unsignedBigInteger("empresa_id")->nullable();
+            $table->foreign("empresa_id")
+                            ->references("id")
+                            ->on("empresas")
+                            ->onUpdate('cascade')
+                            ->onDelete("cascade")
+                            ->nullable();
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('funcionarios');
+    }
+};
