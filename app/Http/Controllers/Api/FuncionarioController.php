928e4ce5fb5023b2d96c6bb202d5bf7d6e97ff63
MaKoTo <matheus.mkodama@gmail.com>
2022-12-03 19:48:00 -0300

    completo

--- app/Http/Controllers/Api/FuncionarioController.php
+++ app/Http/Controllers/Api/FuncionarioController.php
@@ -5,27 +5,120 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Funcionario;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 use App\Http\Requests\StoreFuncionarioRequest;
 use App\Http\Resources\FuncionarioResource;
 
 class FuncionarioController extends Controller
 {
+    
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @OA\Get(
+     *      path="/api/funcionarios",
+     *      operationId="getFuncionarioList",
+     *      tags={"Funcionarios"},
+     *      summary="Retorna a lista de Funcionarios",
+     *      description="Retorna o JSON da lista de Funcionarios",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Operação executada com sucesso"
+     *      )
+     * )
      */
-    public function index()
+    public function index(Request $request)
     {
-        $funcionarios = Funcionario::all();
+       //UNINDO ORDENAÇÃO, PAGINAÇÃO E FILTRO
+       $query = Funcionario::query();
+       $mensagem = "Lista de Funcionarios retornada";
+       $codigoRetorno = 0;
 
-        return response() -> json([
-            'status' => 200,
-            'mensagem' => 'lista retornada',
-            'Lista de Funcionarios' => FuncionarioResource::collection($funcionarios)
-        ], 200);
-    }
+       /*
+        * Realiza processamento do filtro
+        */
+       //Obtem o parametro do filtro
+       $filterParameter = $request->input("filtro");
+
+       //Se não há nenhum parametro
+       if($filterParameter == null){
+           //Retorna todos os produtos e Default
+           $mensagem = "Lista de Funcionarios retornada completa";
+           $codigoRetorno = 200;
+       }else{
+           //Obtem o nome do filtro e o criterio
+           [$filterCriteria, $filterValue] = explode(":",$filterParameter);
+
+           //se o filtro está adequado
+           if($filterCriteria == "nome"){
+               //faz query onde o nome for igual ao passado
+               $funcionarios = $query->where("nome","=",$filterValue);
+               $mensagem = "Lista Funcionarios retornada - filtrada";
+               $codigoRetorno = 200;
+           }else{
+               //Usuario chamou um filtro que não existe, então não há nada a retornar
+               $funcionarios = [];
+               $mensagem = "filtro não aceito";
+               $codigoRetorno = 406;
+           }
+       }
+
+       if($codigoRetorno == 200){
+           /**
+            * Realiza o processamento da ordenação
+            */
+           //Se há um input para ordenação
+           if($request->input('ordenacao','')){
+               $sorts = explode(',',$request->input('ordenacao',''));
+
+               foreach($sorts as $sortColumn){
+                   $sortDirection = Str::startsWith($sortColumn, '-') ? 'desc' : 'asc';
+                   $sortColumn = ltrim($sortColumn, '-');
+
+                   //Transforma os nomes dos parametros em nomes dos campos do modelo
+                   switch($sortColumn){
+                       case("nome");
+                       $query->orderBy('nome', $sortDirection);
+                       break;
+                   }
+               }
+           }
+
+           /**
+            * Realiza o processamento da paginação
+            */
+           $input = $request->input('pagina');
+           if($input){
+               $page = $input;
+               $perPage = 2;//Registro por pagina
+               $query->offset(($page-1) * $perPage)->limit($perPage);
+               $funcionarios = $query->get();
+
+               $recordsTotal = Funcionario::count();
+               $numberOfPages = ceil($recordsTotal / $perPage);
+
+               $mensagem = $mensagem . "+ordenada";
+           }
+
+           //Se o processamento foi OK, retorna com base no criterio
+           if($codigoRetorno == 200){
+               $funcionarios = $query->get();
+               $response = response()->json([
+                   'status' => 200,
+                   'mensagem' => $mensagem,
+                   'empresas' => FuncionarioResource::collection($funcionarios)
+               ],200);
+           }else{
+               //Retorna o erro que ocorreu
+               $response = response()->json([
+                   'status' => 406,
+                   'mensagem' => $mensagem,
+                   'empresas' => $funcionarios
+               ],406);
+           }
+           return $response;
+       }
+   }
+    
 
     /**
      * Show the form for creating a new resource.
@@ -37,11 +130,24 @@ class FuncionarioController extends Controller
         //
     }
 
+
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @OA\Post(
+     *      path="/api/funcionarios",
+     *      operationId="storeFuncionarios",
+     *      tags={"Empresas"},
+     *      summary="Cadastra um novo funcionario",
+     *      description="Retorna o JSON com os dados do novo funcionario",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/StoreFuncionarioRequest")
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Operação executada com sucesso",
+     *          @OA\JsonContent(ref="#/components/schemas/Funcionario")
+     *      )
+     * )
      */
     public function store(StoreFuncionarioRequest $request)
     {
@@ -63,15 +169,46 @@ class FuncionarioController extends Controller
         ],200);
     }
 
+
     /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Funcionario  $funcionario
-     * @return \Illuminate\Http\Response
+     * @OA\Get(
+     *      path="/api/funcionarios/{id}",
+     *      operationId="getFuncionarioById",
+     *      tags={"Funcionarios"},
+     *      summary="Retorna a informação de um Funcionario",
+     *      description="Retorna o JSON do funcionario requisitado",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Id do Funcionario",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Operação executada com sucesso"
+     *      )
+     * )
      */
     public function show(Funcionario $funcionario)
     {
-        //
+        $funcionario = Funcionario::find($funcionario->id);
+
+        if($funcionario){
+            return response()->json([
+                'status' => 200,
+                'mensagem' => "Funcionario retornada",
+                'funcionario' => $funcionario
+            ],200);
+        }else{
+            return response()->json([
+                'status' => 406,
+                'mensagem' => "Funcionario não encontrado",
+                'funcionario' => []
+            ],406);
+        }
     }
 
     /**
@@ -85,12 +222,32 @@ class FuncionarioController extends Controller
         //
     }
 
+
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Funcionario  $funcionario
-     * @return \Illuminate\Http\Response
+     * @OA\Patch(
+     *      path="/api/funcionarios/{id}",
+     *      operationId="updateFuncionario",
+     *      tags={"Funcionarios"},
+     *      summary="Atualiza um Funcionario existente",
+     *      description="Retorna o JSON do Funcionario atualizado",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Id do Funcionario",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/StoreFuncionarioRequest")
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Operação executada com sucesso"
+     *      )
+     * )
      */
     public function update(StoreFuncionarioRequest $request, Funcionario $funcionario)
     {
@@ -112,10 +269,27 @@ class FuncionarioController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Funcionario  $funcionario
-     * @return \Illuminate\Http\Response
+     * @OA\Delete(
+     *      path="/api/funcionarios/{id}",
+     *      operationId="deleteFuncionario",
+     *      tags={"Funcionarios"},
+     *      summary="Apaga um Funcionario existente",
+     *      description="Apaga um Funcionario existente e não há retorno de dados",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Id do Funcionario",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Operação executada com sucesso",
+     *          @OA\JsonContent()
+     *      )
+     * )
      */
     public function destroy(Funcionario $funcionario)
     {
