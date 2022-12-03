e2d0a6b44b9183f8a835fb70a5c1febca2bfe953
PauloHenriqueBentoDosSantos <phbento@outlook.com.br>
2022-12-03 17:33:08 -0300

    feat: CRUD departamento criado

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route:: apiResource('empresas', EmpresaController::class);
+Route:: apiResource('departamentos', \App\Http\Controllers\Api\DepartamentoController::class);
